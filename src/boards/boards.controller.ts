53b8119678f3b4e33454490b4d619a98d1b53629
thursdaycurry <thursdaycurry@gmail.com>
2023-04-16 23:39:50 +0900

    [Fix] add partial template to MVC pattern

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,39 +6,53 @@ import {
   Patch,
   Param,
   Delete,
+  Render,
+  Res,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { Response } from 'express';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
-  @Post()
-  create(@Body() createBoardDto: CreateBoardDto) {
-    console.log('hello');
-    console.log('hello');
-    return this.boardsService.create(createBoardDto);
+  // @Render('news')
+  // @Get('news')
+  // getNewsPage() {}
+
+  @Get('news')
+  getNewsPage(@Res() res: Response) {
+    return res.render('news', { layout: 'news' });
   }
 
-  @Get()
-  findAll() {
-    return this.boardsService.findAll();
-  }
+  @Render('home')
+  @Get('home')
+  getMainPage() {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardsService.findOne(+id);
-  }
+  // @Post()
+  // create(@Body() createBoardDto: CreateBoardDto) {
+  //   return this.boardsService.create(createBoardDto);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardsService.update(+id, updateBoardDto);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.boardsService.findAll();
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.boardsService.remove(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.boardsService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
+  //   return this.boardsService.update(+id, updateBoardDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.boardsService.remove(+id);
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
@@ -11,10 +12,23 @@ async function bootstrap() {
    * for applying MVC pattern
    * _dirname : current path
    */
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-
+  app.useStaticAssets(join(__dirname, '..', 'public')); // CSS, JS static files
+  // app.setBaseViewsDir(join(__dirname, '..', 'views')); // HTML
+  app.engine(
+    'hbs',
+    hbs({
+      extname: 'hbs',
+      defaultLayout: 'main',
+      partialsDir: join(__dirname, '..', 'views/partials'),
+    }),
+  );
   app.setViewEngine('hbs');
+
+  // CORS allow
+  app.enableCors({
+    origin: true, // 모든 url에 개방
+    credentials: true, //프론트에서 credentials 설정 true
+  });
 
   await app.listen(3000);
 }
